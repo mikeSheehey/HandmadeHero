705dababba659caded4c0f9e5f7132784961d452
Sheehey <masheehey@gmail.com>
2026-06-12 09:40:59 -0500

    Finished day 4

--- code/win32_Handmade.cpp
+++ code/win32_Handmade.cpp
@@ -9,6 +9,18 @@ void foo(void)
 		"This is the first thing we have actually printed.\n");
 }
 
+struct projectile
+{
+	//NOTE(sheehey): These are the members, or "fields of this structure!
+
+	char unsigned IsThisOnFire; //NOTE(sheehey): 1 if it's on fire, 0 if it's not
+	int Damage; // NOTE(sheehey): This is how much damage it does on impact
+	int ParticlesPerSecond; // NOTE(sheehey): For special effects
+	short HowManyCooks; // NOTE(sheehey): Too many cooks?
+
+	// 1 byte + 4 bytes + 4 bytes + 2 bytes = 11 bytes
+};
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -32,15 +44,74 @@ int CALLBACK WinMain(
 	Integer = Integer + 5;
 	Integer = 3;
 
+	
+
+	unsigned short Test;
+
+	Test = 500;
+
+	// 0000 0001 1111 0100 = 500
+
 	*/
 
-	char unsigned Test;
-	char unsigned *TestPointer;
+	projectile Test;
 
-	TestPointer = &Test;
+	int SizeOfChar = sizeof(char unsigned);
+	int SizeOfInt = sizeof(int);
+	int SizeOfProjectile = sizeof(projectile);
+	int SizeOfTest = sizeof(Test);
 
-	Test = 255;
-	Test = Test + 1;
+	Test.IsThisOnFire = 1;
+	Test.Damage = 3249839 + Test.IsThisOnFire;
+	Test.ParticlesPerSecond = 23490830;
+	Test.HowManyCooks = 50;
+	
+	//projectile* ProjectilePointer = &Test;
+	//unsigned short* MrPointerMan = (unsigned short*)&Test;
 
-	foo();
-}
+	projectile Projectiles[40];
+
+	int WhichProjectile = 4;
+	Projectiles[WhichProjectile];
+
+	projectile *ProjectilePointer = Projectiles; // Here we are defining projectile pointer to be the starting address of Projectiles
+
+	int SizeOfProjectiles = sizeof(Projectiles); // 16*40
+	int SizeOfProjectilePointer = sizeof(ProjectilePointer); // 4
+
+	// We see Projectiles is just an address to the start of the array but C also understands array 'size'
+
+	Projectiles[30].Damage = 60;
+	(ProjectilePointer + 30)->Damage = 100;
+	((projectile *)((char *)ProjectilePointer + 30 * sizeof(projectile)))->Damage = 100; // These lines are the same but above is compiler helping
+
+	// to see everything these are the operations
+	char *BytePointer = (char *)ProjectilePointer;
+	BytePointer = BytePointer + 30 * sizeof(projectile);
+	projectile *Thirty = (projectile*)BytePointer;
+	Thirty->Damage = 200;
+	// this is the same as line 86.
+	// We first cast our projectile pointer as a char * because it moves 1 byte at a time
+	// Then we move it forward 30 times size of projectile bytes
+	// last we change this address to a projectile * again and assign damage. 
+
+	/* NOTE(sheehey): This is SUPER important. in C for pointer arithmetic say we had
+	
+	sizeof(projectile) == 40
+	sizeof(projectile *) == 8 // starting address of the array and typically 8 bytes on 64-bit machine
+
+	and ProjectilePointer contains address 1000
+
+	then ProjectilePointer + 1 = 1040
+
+	it DOES NOT mean 1000 + sizeof(projectile) = 1000 + 8 = 1008. This is incorrect and not what the compiler does!
+	
+	What the compiler is saying is this is type projectile so since this pointer points to projectiles, when I add one it should
+	move to the next projectile. */
+
+
+
+
+
+
+} 
